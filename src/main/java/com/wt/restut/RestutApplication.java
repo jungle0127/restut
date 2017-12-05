a5ab8d105336e9c4c4c6976c148f4889dde1454d
jungle0127 <arbor@foxmail.com>
2017-12-05 23:25:38 +0800

    added code for the demo

---
+++ src/main/java/com/wt/restut/RestutApplication.java
@@ -0,0 +1,12 @@
+package com.wt.restut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestutApplication.class, args);
+	}
+}
---
+++ src/main/java/com/wt/restut/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.wt.restut.controller;
